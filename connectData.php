37940fe7149e350da8426bb25a134dba00c41404
dcl-jipay <dcl.jeanphig@18pixel.fr>
2019-01-30 12:30:25 +0100

    correction code

--- connectData.php
+++ connectData.php
@@ -1,7 +1,7 @@
 <?php
-		/* On Récupére le contenu du fichier .json */
+/* On Récupére le contenu du fichier .json */
 		$contenu_fichier_json = file_get_contents('databaseConnect.json');
-		/* Les données json sont récupérées sous forme de tableau (true) */
+/* Les données json sont récupérées sous forme de tableau (true) */
 		$dbInfos = json_decode($contenu_fichier_json, true);
 		try{
 			$bdd = new PDO('mysql:host=' . $dbInfos['hostname'] . ';dbname=' . $dbInfos['dbname'], $dbInfos['dbuser'], $dbInfos['dbpassword']);
